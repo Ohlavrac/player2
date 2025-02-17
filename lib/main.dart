d6ffccc8e36677041a8476f87897a52e868f5abe
Ohlavrac <joaovicrcarvalho@gmail.com>
2025-02-17 12:25:17 -0300

    feat: add firebase inicializer in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:player2/app_widget.dart';
+import 'package:player2/firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const AppWidget());
 }
