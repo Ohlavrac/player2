9d9b6fc7e370f4edde196ea2d9a1fff797177963
Ohlavrac <joaovicrcarvalho@gmail.com>
2025-02-17 10:18:37 -0300

    feat: add firebase project

---
+++ lib/data/datasource/auth_remote_datasource_firebase_impl.dart
@@ -0,0 +1,10 @@
+import 'package:player2/data/datasource/auth_remote_datasource.dart';
+import 'package:player2/data/models/user_model.dart';
+
+class AuthRemoteDatasourceFirebaseImpl implements AuthRemoteDatasource {
+  @override
+  Future<UserModel> createNewUser({required UserModel usermodel}) {
+    // TODO: implement createNewUser
+    throw UnimplementedError();
+  }
+}
