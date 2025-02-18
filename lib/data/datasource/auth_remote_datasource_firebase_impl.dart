dedd218f2490c837c010209ba040ad12303ee760
Ohlavrac <joaovicrcarvalho@gmail.com>
2025-02-18 11:36:38 -0300

    feat: add register with email and passw and firestore

--- lib/data/datasource/auth_remote_datasource_firebase_impl.dart
+++ lib/data/datasource/auth_remote_datasource_firebase_impl.dart
@@ -1,9 +1,48 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart' as firebase_auth;
 import 'package:player2/data/datasource/auth_remote_datasource.dart';
 import 'package:player2/data/models/user_model.dart';
 
 class AuthRemoteDatasourceFirebaseImpl implements AuthRemoteDatasource {
+
+  AuthRemoteDatasourceFirebaseImpl({
+    firebase_auth.FirebaseAuth? firebaseAuth,
+  }) : _firebaseAuth = firebaseAuth ?? firebase_auth.FirebaseAuth.instance;
+  
+  final firebase_auth.FirebaseAuth _firebaseAuth;
+  final firebaseDB = FirebaseFirestore.instance;
+
   @override
-  Future<UserModel> createNewUser({required UserModel usermodel}) {
+  Future<UserModel> createNewUser({required UserModel usermodel}) async {
+
+    try {
+      firebase_auth.UserCredential userCredential = await _firebaseAuth.createUserWithEmailAndPassword(
+        email: usermodel.email!,
+        password: usermodel.password!
+      ); 
+
+      if (userCredential.user == null) {
+        throw Exception("Error registering new user");
+      }
+
+      Map<String, dynamic> userData = {
+        "user_id": userCredential.user!.uid,
+        "username": usermodel.username,
+        "description": usermodel.description,
+        "image_url": usermodel.imageUrl,
+        "plataforms": usermodel.platforms,
+        "discord": usermodel.discord,
+        "posts_ids": usermodel.postsIds,
+        "b_day": usermodel.bday,
+        "user_created_at": usermodel.userCreatedAt
+      };
+
+      firebaseDB.collection("users").add(userData).then((DocumentReference doc) => print("Document added with ID: $doc"));
+
+    } catch (error) {
+      throw Exception("Erro while create a user: $error");
+    }
+
     // TODO: implement createNewUser
     throw UnimplementedError();
   }
