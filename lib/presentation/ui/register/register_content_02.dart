8e8357a276e1451f6ec0e2109a555afbe95adeb3
Ohlavrac <joaovicrcarvalho@gmail.com>
2025-02-24 12:59:06 -0300

    feat: add register content 01 and 02

---
+++ lib/presentation/ui/register/register_content_02.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class RegisterContent02 extends StatelessWidget {
+  const RegisterContent02({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
