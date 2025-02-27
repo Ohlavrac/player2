4c1ab0292ad2597c9d08a969a3ae6a9e35e0ca65
Ohlavrac <joaovicrcarvalho@gmail.com>
2025-02-27 09:49:07 -0300

    feat: add validate and update status (username, email, password)

--- lib/presentation/providers/user_provider.dart
+++ lib/presentation/providers/user_provider.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:player2/presentation/status/email_status.dart';
+import 'package:player2/presentation/status/password_status.dart';
+import 'package:player2/presentation/status/username_status.dart';
 
 class UserProvider extends ChangeNotifier {
   String email = "";
@@ -12,6 +15,10 @@ class UserProvider extends ChangeNotifier {
   DateTime bday = DateTime.now();
   DateTime userCreatedAt = DateTime.now();
 
+  UsernameStatus usernameStatus = UsernameStatus.unknow;
+  EmailStatus emailStatus = EmailStatus.unknow;
+  PasswordStatus passwordStatus = PasswordStatus.unknow;
+
   String get getemail => email;
   String get getpassword => password; 
   String get getusername => username; 
@@ -24,17 +31,36 @@ class UserProvider extends ChangeNotifier {
   DateTime get getuserCreatedAt => userCreatedAt;
 
   void setEmail(String value) {
-    email = value;
+    if (value.contains(RegExp(r'^[a-zA-Z0-9._%+-]+@gmail\.com(\.br)?$'))) {
+      emailStatus = EmailStatus.valid;
+      email = value;
+    } else {
+      emailStatus = EmailStatus.invalid;
+    }
     notifyListeners();
   }
 
   void setPassword(String value) {
-    password = value;
+    if (value.length < 8) {
+      passwordStatus = PasswordStatus.short;
+    } else if (value.length >= 8 && value.contains(RegExp(r'[!@#$%&*()_+=-|]'))) {
+      passwordStatus = PasswordStatus.valid;
+    } else {
+      passwordStatus = PasswordStatus.needSymble;
+      password = value;
+    }
     notifyListeners();
   }
 
   void setUsername(String value) {
-    username = value;
+    if (value.length < 3) {
+      usernameStatus = UsernameStatus.invalid;
+    } else if (value.isEmpty || value == "") {
+      usernameStatus = UsernameStatus.unknow;
+    } else {
+      usernameStatus = UsernameStatus.valid;
+      username = value;
+    }
     notifyListeners();
   }
 
